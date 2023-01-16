7f2fc89718b753433788a102fdc222441ec6be88
VICKY <vishujoshi21nov2000@gmail.com>
2023-01-16 14:42:15 +0530

    Added Modules

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: ['./dist/*.html', './dist/*.js'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            height: {
+                "100": '35rem'
+            },
+            fontFamily: {
+                "JB": "JetBrains Mono, monospace"
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
